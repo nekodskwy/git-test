bd72f49a74d51f6d68cb127d1a7ae94e2cf0518c
nekodskwy <114984332+nekodskwy@users.noreply.github.com>
2022-10-04 20:41:11 +0800

    pull test

--- src/main/java/com/nekodskwy/Application.java
+++ src/main/java/com/nekodskwy/Application.java
@@ -11,6 +11,7 @@ public class Application {
         System.out.println("hot-fix test");
         System.out.println("push test!");
         System.out.println("push test!!!");
+        System.out.println("pull test!");
     }
 
 }
